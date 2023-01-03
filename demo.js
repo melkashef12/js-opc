c4b30e16a52dcae160d4597c1888ebffb2eec82c
Mohamed El Kashef <melkashef12@gmail.com>
2023-01-03 15:43:25 +0100

    [Creating Javascript Objects] Créer une fonction constructeur permettant d’initialiser une personne

--- demo.js
+++ demo.js
@@ -8,14 +8,17 @@
     isAdult() {return this.age>18}
   }
 
-
-  let healthStats = {
-    height : 174,
-    weight : 78
+  // Une fonction constructeur est une fonction normale qui fait référence à this.
+  // this est le contexte dans lequel la fonction s'éxécute.
+  function Person(firstName,lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.isAdult =function() {return this.age > 18; }
   }
 
-  display(Object.assign({},person,healthStats)); // Toujours utiliser un objet vide en premier paramètre pour préserver l'immuabilité
-
-  display(person);
+  // new crée implicitement un Object et l'assigne à this pour qu'il devienne le contexte d'exécution de la fonction constructeur
+  display(new Person("Mo", "El Kashef",36).isAdult());
+  display(new Person("Dahlia", "El Kashef",5).isAdult());
 
 })();
